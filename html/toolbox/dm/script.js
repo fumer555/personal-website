590b33ac3d1e58c3e08c5b46347c78c1fb30599b
Frank <zi.h.yin@mail.mcgill.ca>
2024-07-31 02:41:31 -0400

    test

--- html/toolbox/dm/script.js
+++ html/toolbox/dm/script.js
@@ -1,26 +1,48 @@
-function createPolylines(startY, incrementX, numLines, classList) {
-    const svgNS = "http://www.w3.org/2000/svg";
-    let y = startY;
-    for (let i = 0; i < numLines; i++) {
-        let polyline = document.createElementNS(svgNS, "polyline");
-        polyline.setAttribute("points", `265,${y} ${265 + incrementX},${y}`);
-        polyline.setAttribute("id", `line-${i}`);
-        if (classList && classList[i]) {
-            polyline.setAttribute("class", classList[i]);
+class System {
+    constructor(svgRootId) {
+        this.svgNS = "http://www.w3.org/2000/svg";
+        this.svgRoot = document.getElementById(svgRootId);
+    }
+
+    createPolylines(startY, incrementX, numLines, classList) {
+        // const svgNS = "http://www.w3.org/2000/svg";
+        let y = startY;
+        for (let i = 0; i < numLines; i++) {
+            let polyline = document.createElementNS(svgNS, "polyline");
+            polyline.setAttribute("points", `265,${y} ${265 + incrementX},${y}`);
+            polyline.setAttribute("id", `line-${i}`);
+            if (classList && classList[i]) {
+                polyline.setAttribute("class", classList[i]);
+            }
+            document.getElementById("svgRoot").appendChild(polyline);
+            y += 40; // Increment y by 40 for each line
         }
-        document.getElementById("svgRoot").appendChild(polyline);
-        y += 40; // Increment y by 40 for each line
+    }
+
+    changeClass(lineId, newClass) {
+        let line = document.getElementById(lineId);
+        if (line) {
+            line.setAttribute("class", newClass);
+        }
     }
 }
 
-function changeClass(lineId, newClass) {
-    let line = document.getElementById(lineId);
-    if (line) {
-        line.setAttribute("class", newClass);
-    }
-}
+
+
+// function changeClass(lineId, newClass) {
+//     let line = document.getElementById(lineId);
+//     if (line) {
+//         line.setAttribute("class", newClass);
+//     }
+// }
 
 // Initialize with some lines
-window.onload = function() {
-    createPolylines(855, 290, 12, ["", "dashed-line", "hidden", "", "", "", "", "", "", "", "", ""]);
-};
+// window.onload = function() {
+//     createPolylines(855, 290, 12, ["", "dashed-line", "hidden", "", "", "", "", "", "", "", "", ""]);
+// };
+
+// Using the class
+document.addEventListener("DOMContentLoaded", () => {
+    const lineManager = new SVGLineManager('svgRoot');
+    lineManager.createPolylines(855, 290, 12, ["", "dashed-line", "hidden", "", "", "", "", "", "", "", "", ""]);
+});
