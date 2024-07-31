4c3be6cd26ad058b7a0e715836ef25d2ba9ecc92
Frank <zi.h.yin@mail.mcgill.ca>
2024-07-31 02:51:39 -0400

    test

--- html/toolbox/dm/script.js
+++ html/toolbox/dm/script.js
@@ -1,20 +1,19 @@
-class System {
+class SVGLineManager {
     constructor(svgRootId) {
         this.svgNS = "http://www.w3.org/2000/svg";
         this.svgRoot = document.getElementById(svgRootId);
     }
 
     createPolylines(startY, incrementX, numLines, classList) {
-        // const svgNS = "http://www.w3.org/2000/svg";
         let y = startY;
         for (let i = 0; i < numLines; i++) {
-            let polyline = document.createElementNS(svgNS, "polyline");
+            let polyline = document.createElementNS(this.svgNS, "polyline");
             polyline.setAttribute("points", `265,${y} ${265 + incrementX},${y}`);
             polyline.setAttribute("id", `line-${i}`);
             if (classList && classList[i]) {
                 polyline.setAttribute("class", classList[i]);
             }
-            document.getElementById("svgRoot").appendChild(polyline);
+            this.svgRoot.appendChild(polyline);
             y += 40; // Increment y by 40 for each line
         }
     }
@@ -27,25 +26,8 @@ class System {
     }
 }
 
-
-
-// function changeClass(lineId, newClass) {
-//     let line = document.getElementById(lineId);
-//     if (line) {
-//         line.setAttribute("class", newClass);
-//     }
-// }
-
-// Initialize with some lines
-window.onload = function() {
-
-    const lineManager = new System('svgRoot');
-    lineManager.createPolylines(855, 290, 12, ["", "dashed-line", "hidden", "hidden", "", "", "", "", "", "", "", ""]);
-    // createPolylines(855, 290, 12, ["", "dashed-line", "hidden", "", "", "", "", "", "", "", "", ""]);
-};
-
 // Using the class
-// document.addEventListener("DOMContentLoaded", () => {
-//     const lineManager = new System('svgRoot');
-//     lineManager.createPolylines(855, 290, 12, ["", "dashed-line", "hidden", "hidden", "", "", "", "", "", "", "", ""]);
-// });
+document.addEventListener("DOMContentLoaded", () => {
+    const lineManager = new SVGLineManager('svgRoot');
+    lineManager.createPolylines(855, 290, 12, ["", "dashed-line", "hidden", "", "", "", "", "", "", "", "", ""]);
+});
